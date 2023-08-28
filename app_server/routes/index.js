c082d359072d565209acaa76d9f3bcf2e2f7b094
Samoilenko <ssamikk@mail.ru>
2023-08-28 20:05:04 +0530

    Signed-off-by: Samoilenko <ssamikk@mail.ru>

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
-var ctrlMain = require('../controllers/main');
+var ctrlLocation = require('../controllers/location');
+var ctrlOthers = require('../controllers/others');
+
+/* GET страницы местоположения. */
+router.get('/', ctrlLocation.homeList);
+router.get('/location', ctrlLocation.locationInfo);
+router.get('/location/review/new', ctrlLocation.addReview);
 
 
-/* GET home page. */
-router.get('/', ctrlMain.index);
+//Другие страницы
+router.get('/about', ctrlOthers.about);
 
 module.exports = router;
